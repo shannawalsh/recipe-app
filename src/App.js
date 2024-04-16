52dd2fb476c16449fe8f6449eb5d1a3002e2660c
Shanna <shanna.r.walsh@gmail.com>
2024-04-16 17:30:53 -0500

    Add new recipe form for the front end

--- src/App.js
+++ src/App.js
@@ -3,21 +3,34 @@ import Header from "./components/Header";
 import "./App.css";
 import React, { useState, useEffect } from 'react';
 import RecipeExcerpt from "./components/RecipeExcerpt";
+import NewRecipeForm from "./components/NewRecipeForm";
 
 function App() {
   const [recipes, setRecipes] = useState([]);
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
+  const [newRecipe, setNewRecipe] = useState(
+    {
+      title: "",
+    ingredients: "",
+    instructions: "",
+    servings: 1, // conservative default
+    description: "",
+    image_url: "https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" //default
+    }
+  );
+  const [showNewRecipeForm, setShowNewRecipeForm] = useState(false);
 
   const fetchAllRecipes = async () => {
     try {
       const response = await fetch("/api/recipes");
-      const results = await response.json();
       if (response.ok) { 
-      setRecipes(results);
+        const results = await response.json();
+        setRecipes(results);
       } else {
-        console.log(results);
+        console.log("Opps = could not fetch any recipes!");
       }
-    } catch (error) {
-      console.log("Something went wrong", error);
+    } catch (e) {
+      console.log("Something went wrong", e);
     }
   };
 
@@ -25,9 +38,24 @@ function App() {
     fetchAllRecipes();
   }, []);
 
+  const hideRecipeForm = () => {
+    setShowNewRecipeForm(false);
+  };
+
+  const showRecipeForm = () => {
+    setShowNewRecipeForm(true);
+    setSelectedRecipe(null);
+  };
+
+  const onUpdateForm = (e) => {
+    const { name, value } = e.target;
+    setNewRecipe({ ...newRecipe, [name]: value});
+  };
+
   return (
     <div className='recipe-app'>
-      <Header />
+      <Header showRecipeForm={showRecipeForm} />
+      {showNewRecipeForm && <NewRecipeForm newRecipe={newRecipe} hideRecipeForm={hideRecipeForm} onUpdateForm={onUpdateForm}  />}
       <div className="recipe-list">
       {recipes.map((recipe) => (
         <RecipeExcerpt key={recipe.id} recipe={recipe} />
