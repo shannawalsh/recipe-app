56a88c9bb107a905738934488d42e49cb8744fff
Shanna <shanna.r.walsh@gmail.com>
2024-04-15 20:03:08 -0500

    Updated the React front end to pull the recipes in the database.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from "react";
+//import React from "react";
 import Header from "./components/Header";
 import "./App.css";
+import React, { useState, useEffect } from 'react';
+import RecipeExcerpt from "./components/RecipeExcerpt";
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+
+  const fetchAllRecipes = async () => {
+    try {
+      const response = await fetch("/api/recipes");
+      const results = await response.json();
+      if (response.ok) { 
+      setRecipes(results);
+      } else {
+        console.log(results);
+      }
+    } catch (error) {
+      console.log("Something went wrong", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllRecipes();
+  }, []);
+
   return (
     <div className='recipe-app'>
       <Header />
+      <div className="recipe-list">
+      {recipes.map((recipe) => (
+        <RecipeExcerpt key={recipe.id} recipe={recipe} />
+        ))}
+      </div>
       <p>Your recipes here! </p>
     </div>
   );
