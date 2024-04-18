1f65dc6eb689da5e8fe2a6567187d308d61cdb4f
Shanna <shanna.r.walsh@gmail.com>
2024-04-17 20:42:43 -0500

    created component to show full recipe

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import "./App.css";
 import React, { useState, useEffect } from 'react';
 import RecipeExcerpt from "./components/RecipeExcerpt";
 import NewRecipeForm from "./components/NewRecipeForm";
+import RecipeFull from "./components/RecipeFull";
+
 
 function App() {
   const [recipes, setRecipes] = useState([]);
   const [selectedRecipe, setSelectedRecipe] = useState(null);
+  const [showNewRecipeForm, setShowNewRecipeForm] = useState(false);
+
   const [newRecipe, setNewRecipe] = useState(
     {
       title: "",
@@ -18,25 +22,31 @@ function App() {
     image_url: "https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" //default
     }
   );
-  const [showNewRecipeForm, setShowNewRecipeForm] = useState(false);
-
-  const fetchAllRecipes = async () => {
-    try {
-      const response = await fetch("/api/recipes");
-      if (response.ok) { 
-        const results = await response.json();
-        setRecipes(results);
-      } else {
-        console.log("Opps = could not fetch any recipes!");
-      }
-    } catch (e) {
-      console.log("Something went wrong", e);
-    }
-  };
-
+  
   useEffect(() => {
+    const fetchAllRecipes = async () => {
+      try {
+        const response = await fetch("/api/recipes");
+        if (response.ok) { 
+          const results = await response.json();
+          setRecipes(results);
+        } else {
+          console.log("Opps = could not fetch any recipes!");
+        }
+      } catch (e) {
+        console.log("Something went wrong", e);
+      }
+    };
     fetchAllRecipes();
   }, []);
+
+  const handleSelectRecipe = (recipe) => {
+    setSelectedRecipe(recipe);
+  };
+
+  const handleUnselectRecipe = () => {
+    setSelectedRecipe(null);
+  };
 
   const hideRecipeForm = () => {
     setShowNewRecipeForm(false);
@@ -56,12 +66,14 @@ function App() {
     <div className='recipe-app'>
       <Header showRecipeForm={showRecipeForm} />
       {showNewRecipeForm && <NewRecipeForm newRecipe={newRecipe} hideRecipeForm={hideRecipeForm} onUpdateForm={onUpdateForm}  />}
+      {selectedRecipe && <RecipeFull selectedRecipe={selectedRecipe} handleUnselectRecipe={handleUnselectRecipe} />}
+      {!selectedRecipe && (
       <div className="recipe-list">
       {recipes.map((recipe) => (
-        <RecipeExcerpt key={recipe.id} recipe={recipe} />
+        <RecipeExcerpt key={recipe.id} recipe={recipe} handleSelectRecipe={handleSelectRecipe} />
         ))}
       </div>
-      <p>Your recipes here! </p>
+      )}
     </div>
   );
 }
